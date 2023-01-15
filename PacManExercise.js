6e57311c6b81994cff20e14d1af4bdf77c484918
Marisela Ruiz <115127240+marisela-ruiz@users.noreply.github.com>
2023-01-14 22:25:52 -0600

    Add files via upload

---
+++ PacManExercise.js
@@ -0,0 +1,59 @@
+var pos = 0;
+    const pacArray = [
+        ['PacMan1.png', 'PacMan2.png'],
+        ['PacMan3.png', 'PacMan4.png']
+    ];
+    var direction = 0;
+    const pacMen = [];
+
+    function setToRandom(scale) {
+        return {
+            x: Math.random() * scale,
+            y: Math.random() * scale
+        }
+    }
+    // Factory to make a PacMan 
+    function makePac() {
+        // returns an object with values scaled {x: 33, y: 21}
+        let velocity = setToRandom(10);
+        let position = setToRandom(200);
+        // Add image to div id = game
+        let game = document.getElementById('game');
+        let newimg = document.createElement('img');
+        newimg.style.position = 'absolute';
+        newimg.src = 'PacMan1.png';
+        newimg.width = 100;
+        newimg.style.left = position.x;
+        newimg.style.top = position.y;
+        game.appendChild(newimg);
+        // new style of creating an object
+        return {
+            position,
+            velocity,
+            newimg
+        }
+    }
+
+    function update() {
+        //loop over pacmen array and move each one and move image in DOM
+        pacMen.forEach((item) => {
+            checkCollisions(item)
+            item.position.x += item.velocity.x;
+            item.position.y += item.velocity.y;
+
+            item.newimg.style.left = item.position.x;
+            item.newimg.style.top = item.position.y;
+        })
+        setTimeout(update, 20);
+    }
+
+    function checkCollisions(item) {
+        if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+            item.position.x + item.velocity.x < 0) item.velocity.x = -item.velocity.x;
+        if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
+            item.position.y + item.velocity.y < 0) item.velocity.y = -item.velocity.y;
+    }
+
+    function makeOne() {
+        pacMen.push(makePac()); // add a new PacMan
+    }
